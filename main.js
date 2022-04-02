9587e39365c5e5ed3901690d7f31a9a654e47383
Melek Delibas <melekdondudelibas@gmail.com>
2022-04-02 23:55:26 +0300

    LocalStorage eklendi

--- main.js
+++ main.js
@@ -4,16 +4,34 @@ const listDOM = document.querySelector("#list");
 const toastLiveSuccess = document.querySelector('#liveToastSuccess')
 const toastLiveError = document.querySelector('#liveToastError')
 
+let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+localStorage.setItem('items', JSON.stringify(itemsArray));
+const data = JSON.parse(localStorage.getItem('items'));
 
 //element ekleme ve "eklendi"/"boş ekleme yapılamaz" uyarıları
 function newElement() {  
     let deger = taskDOM.value.trim();
     if (deger) {
+        let number = itemsArray.length > 0 ? itemsArray[itemsArray.length-1].id + 1 : 1
+        itemsArray.push({info: deger, id: number});
+        localStorage.setItem('items', JSON.stringify(itemsArray));
+        liMaker(deger,number)
         var toast = new bootstrap.Toast(toastLiveSuccess)
         toast.show()
 
+    } else {
+        var toast = new bootstrap.Toast(toastLiveError)
+        toast.show()
+    }
+    
+}
+
+//li oluşturma
+const liMaker = (deger,id) => {
+
         let liDOM = document.createElement('li');
         liDOM.innerHTML = deger
+        liDOM.id=id;
         listDOM.appendChild(liDOM);
         taskDOM.value = "";
 
@@ -30,16 +48,15 @@ function newElement() {
             liElement[i].onclick = check;
 
         }
-
-    } else {
-        var toast = new bootstrap.Toast(toastLiveError)
-        toast.show()
-    }
-    
 }
+data.forEach(item => {
+    liMaker(item.info,item.id);
+});
 
 //element silme fonksiyonu
 function removeButton() {
+    itemsArray = itemsArray.filter(item => item.id != this.parentElement.id)
+    localStorage.setItem('items', JSON.stringify(itemsArray));
     this.parentElement.remove();
 }
 function check() {
